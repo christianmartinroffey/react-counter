afa202410ddc608c3c3ced7170b33163d0b4b408
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-27 07:44:22 +0000

    basic counter

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,33 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SimpleCounter from "./component/Counter.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+
+setInterval(function () {
+	const six = Math.floor(counter / 100000);
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+
+	ReactDOM.render(
+		// only need to do once
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
+
+// ReactDOM.render(<Home />, document.querySelector("#app"));
